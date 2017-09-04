29b6fcbfebe9a4e7dc1878dc96b812ec73f4138b
alanionita <a.g.ionita@gmail.com>
2017-09-04 15:45:27 +0100

    [setup] added basic connection to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
+/* eslint-disable no-console */
+
+if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
+
+mongoose.connect(db, (err) => {
+  if (!err) {
+    console.log(`connected to the Database: ${db}`);
+  } else {
+    console.log(`error connecting to the Database ${err}`);
+  }
+});
 
 const app = express();
 app.get('/', (req, res) => {
   res.status(200).send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
 
 module.exports = app;
