e8217f9e3b62a10d40ee6bb1addddab02592896f
alanionita <a.g.ionita@gmail.com>
2017-09-04 16:02:35 +0100

    [models] added models index

---
+++ models/models.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  Users: path.resolve(__dirname, 'Users'),
+  Articles: path.resolve(__dirname, 'Articles'),
+  Comments: path.resolve(__dirname, 'Comments'),
+  Topics: path.resolve(__dirname, 'Topics'),
+};
