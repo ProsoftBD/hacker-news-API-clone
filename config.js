1eeedeae7948aeb51a48a0bfddf074313d9a1d49
alanionita <a.g.ionita@gmail.com>
2017-09-04 15:44:56 +0100

    [setup] added config file determining which DB to use

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  DB: {
+    test: 'mongodb://127.0.0.1:27017/hacker-news-clone-api-test',
+    dev: 'mongodb://127.0.0.1:27017/hacker-news-clone-api',
+  },
+  PORT: {
+    test: 3090,
+    dev: 3000,
+  },
+};
