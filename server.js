d75af9da595b51b10e93c61d330f86bdc0ac0ec5
alanionita <a.g.ionita@gmail.com>
2017-09-04 17:32:05 +0100

    [router] added basic router and confirmed functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
 const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config');
+const routes = require('./routers/router');
 
 const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 const PORT = config.PORT[process.env.NODE_ENV] || process.env.PORT;
@@ -19,8 +20,13 @@ mongoose.connect(db, (err) => {
 
 const app = express();
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  res
+    .status(200)
+    .send('<h1>Welcome to the Hacker News Clone API</h1> \n Go to /api for more...')
+    .send();
 });
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
--- spec/main.spec.js
+++ spec/main.spec.js
@@ -12,7 +12,20 @@ describe('API', () => {
           if (err) done(err);
           else {
             expect(res.status).to.equal(200);
-            expect(res.text).to.equal('Hello World!');
+            expect(res.text).to.equal('<h1>Welcome to the Hacker News Clone API</h1> \n Go to /api for more...');
+            done();
+          }
+        });
+    });
+  });
+  describe('GET /api/', () => {
+    it('responds with status code 200', (done) => {
+      request(server)
+        .get('/api/')
+        .end((err, res) => {
+          if (err) done(err);
+          else {
+            expect(res.status).to.equal(200);
             done();
           }
         });
